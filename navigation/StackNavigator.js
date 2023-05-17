81d105d0bdabf1bb4dcabf1d02f55f76b39d94a1
Khulon <e0725788@u.nus.edu>
2023-05-17 15:35:29 +0800

    nav changes

--- navigation/StackNavigator.js
+++ navigation/StackNavigator.js
@@ -1,23 +1,21 @@
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack';
-import LoginScreen from '../screens/authentication/LoginScreen';
+import StartScreen from '../screens/authentication/StartScreen';
 import RegistrationScreen from '../screens/authentication/RegistrationScreen';
 
 
 const Stack = createStackNavigator();
 
 
-const StackNavigator = () =>{
-
+export default function StackNavigator() {
     return (
         <Stack.Navigator initialRouteName="LoginScreen">
-            <Stack.Screen name="LoginScreen" component={LoginScreen} options={{headerShown: false}}/> 
+            <Stack.Screen name="LoginScreen" component={StartScreen} options={{headerShown: false}}/> 
             <Stack.Screen name="RegistrationScreen" component={RegistrationScreen} options={{headerShown: false}}/> 
         </Stack.Navigator>
     );
 }
 
-export default StackNavigator;
 
 
 
