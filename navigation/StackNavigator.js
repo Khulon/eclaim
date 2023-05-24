c857d99cefc34f2434e5f8890424a0bd6d978d4d
Guok Wei Jie <guokweijie@gmail.com>
2023-05-24 10:26:25 +0800

    Login settled

--- navigation/StackNavigator.js
+++ navigation/StackNavigator.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { createStackNavigator } from '@react-navigation/stack';
 import AuthenticationStack from './AuthenticationStack';
+import useAuth from '../hooks/useAuth';
 import HomeStack from './HomeStack';
 import AdminStack from './AdminStack';
 
@@ -8,13 +9,13 @@ const Stack = createStackNavigator();
     
 
 export default function StackNavigator() {
-    const [user, setUser] = useState(false);
-    const [userType, setUserType] = useState(true);
+    const {user, setUser} = useAuth();
+    const { userType, setUserType } = useAuth();
 
     return (
         <Stack.Navigator>
         {user ? (
-            userType ? (
+            userType.userType == 'Admin' ? (
                 <Stack.Screen name="AdminStack" component={AdminStack} options={{headerShown: false}}/> 
             ):(
                 <Stack.Screen name="HomeStack" component={HomeStack} options={{headerShown: false}}/> 
