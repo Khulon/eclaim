936eb7e2a455dea8563b5165d6f5f01ca5e3811f
Khulon <88884346+Khulon@users.noreply.github.com>
2023-05-29 09:06:47 +0800

    bottom nav

--- navigation/BottomTabNavigator.js
+++ navigation/BottomTabNavigator.js
@@ -1,7 +1,8 @@
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, Animated } from 'react-native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from "react-native-vector-icons";
-import React from 'react'
+import React, {useRef, useState} from 'react'
+import { MoveNegAnimation, MovePosAnimation } from '../assets/animation/AllAnimations';
 
 import HomeStack from './HomeStack';
 import ProfileStack from './ProfileStack';
@@ -11,31 +12,52 @@ import MyClaimsStack from './MyClaimsStack';
 const Tab = createBottomTabNavigator();
 
 export default function BottomTabNavigator() {
+  const [isBackButtonHover, setIsBackButtonHover] = useState(false);
+  const loginButtonHover = useRef(new Animated.Value(0)).current;
+
   const CustomTabBarButton = ({children, onPress})=>(
     <TouchableOpacity className="flex justify-center items-center bg-[#D9D9D9]">
-      <Icon type="octicon" name="plus"/>
+      <Ionicons name="add-circle-outline" color={color} size={size}/>
     </TouchableOpacity>
   );
 
   return (
     <Tab.Navigator
+
       screenOptions={{
+        tabBarStyle: { 
+          height:'70px',
+          alignItems:'center'
+        },
+
+      tabBarItemStyle: {
+        borderRadius:'12px',
+        width:"60px",
+        height:'50px',
+      },
+
         headerShown: false,
         tabBarShowLabel: false,
-        tabBarActiveTintColor: '#39C7A5',
+        tabBarActiveTintColor: '#E04F4F',
+        tabBarActiveBackgroundColor: '#EEEEEE',
+        tabBarInactiveTintColor: '#444444'
+      
       }}
     >
         <Tab.Screen name="Home" component={HomeStack}
           options={{
+            styles: {transform: [{translateY: loginButtonHover}]},
             tabBarIcon: ({focused, color, size})=>(
-                <Ionicons name="chevron-back-outline" color={color} size={size}/>
+              <View style={{postion:'absolute',transform: [{translateY: loginButtonHover}]}} onMouseEnter={() => MoveNegAnimation(loginButtonHover)} onMouseLeave={() => MovePosAnimation(loginButtonHover)}>
+                <Ionicons name="home-outline" color={color} size={size}/>
+              </View>
             ),
           }}
         />
-        <Tab.Screen name="Chat" component={ManagementStack}
+        <Tab.Screen name="Management" component={ManagementStack}
           options={{
             tabBarIcon: ({focused, color, size})=>(
-                <Ionicons name="chevron-back-outline" color={color} size={size}/>
+                <Ionicons name="file-tray-full-outline" color={color} size={size}/>
             ),
           }}
         />
@@ -49,25 +71,26 @@ export default function BottomTabNavigator() {
           options={{
             tabBarIcon: ({focused, color, size})=>(
               <View className="rounded-xl bg-[#BFEDE3] w-10 h-8 items-center justify-center">
-                <Ionicons name="chevron-back-outline" color={color} size={size}/>
+                <Ionicons name="duplicate-outline" color={color} size={size}/>
               </View>
             ),
           }}
         />
-        <Tab.Screen name="Cart" component={MyClaimsStack}
+        <Tab.Screen name="MyClaims" component={MyClaimsStack}
           options={{
             tabBarIcon: ({focused, color, size})=>(
-                <Ionicons name="chevron-back-outline" color={color} size={size}/>
+                <Ionicons name="document-text-outline" color={color} size={size}/>
             ),
           }}
         />
         <Tab.Screen name="Profile" component={ProfileStack}
           options={{
             tabBarIcon: ({focused, color, size})=>(
-                <Ionicons name="chevron-back-outline" color={color} size={size}/>
+                <Ionicons name="people-outline" color={color} size={size}/>
             ),
           }}
         />
     </Tab.Navigator>
+
   )
 }
