34dbf94dddf19c4c265624764acf22da893e380d
Guok Wei Jie <guokweijie@gmail.com>
2023-06-23 16:56:36 +0800

    Fix convert excel

--- screens/management/ViewManagedClaimsScreen.js
+++ screens/management/ViewManagedClaimsScreen.js
@@ -450,7 +450,7 @@ export default function ViewManagedClaimsScreen({ navigation, route}) {
             <View style={{width:'23%', flexDirection:'row', justifyContent:'center'}}>
               <View style={{width:'40px', alignItems:'center'}}>
                 <TouchableOpacity style={{flexDirection: "row", alignItems: "center"}} onMouseEnter={() => setIsDownloadExcelButtonHover(true)} onMouseLeave={() => setIsDownloadExcelButtonHover(false)} 
-                onPress={() => excel()}>
+                onPress={() => excel(claim, table)}>
                 <View style={styles.downloadButton}>
                   {isDownloadExcelButtonHover?(
                     <Text><FontAwesome name="file-excel-o" color="#3F9E87" size="27px"/></Text>
