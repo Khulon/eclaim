ce6ea373bfe287e94296c3c93276003672cd7d5d
Khulon <88884346+Khulon@users.noreply.github.com>
2023-06-26 08:59:44 +0800

    finish pdf printing

--- screens/management/pdf.js
+++ screens/management/pdf.js
@@ -60,6 +60,7 @@ export default function pdf({route}) {
     useEffect(() => {
         if (hide) {
           print()
+          setHide(false)
         }
         
       }, [hide]);
@@ -72,22 +73,24 @@ export default function pdf({route}) {
     return (
         <View style={{width:'100%', flexGrow:1, alignItems:'center'}}>
                 {hide ? (
-                    <View style={{position:'absolute', zIndex:999}}>
-                        <TouchableOpacity onPress={()=>setHide(false)}>
-                            <Text> <Text><Ionicons name="chevron-back-outline" color="white" size='25px'/></Text> </Text>
-                        </TouchableOpacity>
+                    <View>
                     </View>
                 ) : (
-                    <View style={{alignItems:'center', justifyContent:'space-between', flexDirection:'row', height:'70px', borderWidth:'3px', borderColor:'black', width:'400px', borderRadius:'20px', position:'absolute', opacity:'0.5', backgroundColor:'white', zIndex:999}}>
+                    <View style={{alignItems:'center', justifyContent:'space-between', flexDirection:'row', height:'200px', borderWidth:'3px', borderColor:'black', width:'450px', borderRadius:'20px', position:'absolute', opacity:'0.5', backgroundColor:'white', zIndex:999}}>
                     <TouchableOpacity onPress={()=>options != 0 ? setOptions(options-1) : 0} style={{paddingHorizontal:'20px'}}>
-                        <Text> <Text><Ionicons name="chevron-back-outline" color="#444" size='25px'/></Text> </Text>
+                        <Text> <Text><Ionicons name="chevron-back-outline" color="#444" size='30px'/></Text> </Text>
                     </TouchableOpacity>
-                    <TouchableOpacity style={{alignItems:'center'}} onPress={()=> handlePrint()}>
-                        <Text style={{fontWeight:600, fontSize:'17px'}}>{presets[options].receiptsPerPage} per page</Text>
-                        <Text style={{fontWeight:400, fontSize:'13px'}}>{presets[options].layout}</Text>
-                    </TouchableOpacity>
+                    <View style={{width:'200px', justifyContent:'center', alignItems:'center'}}>
+                        <Text style={{fontWeight:600, fontSize:'25px'}}>{presets[options].receiptsPerPage} per page</Text>
+                        <Text style={{fontWeight:400, fontSize:'20px'}}>Click print, or ctrl-P, </Text>
+                        <Text style={{fontWeight:400, fontSize:'20px'}}>then select layout </Text>
+                        <Text style={{fontWeight:400, fontSize:'20px'}}>options ({presets[options].layout})</Text>
+                        <TouchableOpacity style={{alignItems:'center'}} onPress={()=> handlePrint()}>
+                            <Text> <Text><Ionicons name="print-outline" color="#444" size='30px'/></Text> </Text>
+                        </TouchableOpacity>
+                    </View>
                     <TouchableOpacity onPress={()=>options != 6 ? setOptions(options+1) : 6} style={{paddingHorizontal:'20px'}}>
-                        <Text> <Text><Ionicons name="chevron-forward-outline" color="#444" size='25px'/></Text> </Text>
+                        <Text> <Text><Ionicons name="chevron-forward-outline" color="#444" size='30px'/></Text> </Text>
                     </TouchableOpacity>
                     </View>
                 )}
