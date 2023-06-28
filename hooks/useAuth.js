08802b2f233a507f1b8516f4de525d62cb4bd608
Khulon <88884346+Khulon@users.noreply.github.com>
2023-06-28 17:00:04 +0800

    Deploying website

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -7,7 +7,7 @@ export const AuthProvider = ({children}) => {
 
   async function loginUser (loginDetails) {
     const header = { 'Accept': 'application/json','Content-Type': 'application/json' };
-    await fetch('http://localhost:5000/login', {
+    await fetch('http://10.0.1.28:5000/login', {
       method: 'POST', 
       headers: header,
       body: JSON.stringify(loginDetails)})
